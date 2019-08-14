6cf311f2f8819a9fba3ac64981a030efcca5d11c
Matthew Wright <matthewbwright@gmail.com>
2019-08-13 21:20:51 -0500

    setting up structure and adding some content to the home page

---
+++ wrightlawgroup/src/Components/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Jumbotron, Carousel, Container, Row, Col, Image, Button } from 'react-bootstrap';
+import './Home.css';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <Container>
+        <Jumbotron>
+          <h2>Welcome to Wright Law Group!</h2>
+          <p>This website is being built with React, React-Router, and React-Bootstrap</p>
+        </Jumbotron>
+        <Link to="/about">
+          <Button bsStyle="primary">About</Button>
+        </Link>
+      </Container>
+    )
+  }
+}
