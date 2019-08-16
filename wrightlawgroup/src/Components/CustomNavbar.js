66268056f8e062c183718d0f6ccfbed8e6491216
Matthew Wright <matthewbwright@gmail.com>
2019-08-15 23:37:42 -0500

    finishes out home page

--- wrightlawgroup/src/Components/CustomNavbar.js
+++ wrightlawgroup/src/Components/CustomNavbar.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+import './CustomNavbar.css';
+
 const CustomNavbar = () => {
   
     return (
-      <Navbar bg="dark" variant="dark" default collapseOnSelect>
+      <Navbar  default collapseOnSelect>
         
           <Navbar.Brand>
             <Link to="/">Wright Law Group</Link>
@@ -13,16 +15,16 @@ const CustomNavbar = () => {
           <Navbar.Toggle />
         
         <Navbar.Collapse>
-          <Nav pullRight color="white">
-            <NavItem eventkey={1} componentClass={Link} href="/" to="/">
+          <Nav pullRight>
+            <Nav.Link eventkey={1} componentClass={Link} href="/" to="/">
               Home
-            </NavItem>
-            <NavItem eventkey={2} componentClass={Link} href="/about" to="/about">
+            </Nav.Link>
+            <Nav.Link eventkey={2} componentClass={Link} href="/about" to="/about">
               About
-            </NavItem>
-            <NavItem eventkey={3} componentClass={Link} href="/news" to="/news">
+            </Nav.Link>
+            <Nav.Link eventkey={3} componentClass={Link} href="/news" to="/news">
               News
-            </NavItem>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
--- wrightlawgroup/src/Components/Home.js
+++ wrightlawgroup/src/Components/Home.js
@@ -10,10 +10,28 @@ const Home = () => {
         <Jumbotron>
           <h2>Welcome to Wright Law Group!</h2>
           <p>This website is being built with React, React-Router, and React-Bootstrap</p>
-        </Jumbotron>
-        <Link to="/about">
+          <Link to="/about">
           <Button bsStyle="primary">About</Button>
         </Link>
+        </Jumbotron>
+        <Row className="show-grid text-center">
+          <Col xs={12} sm={4} className="person-wrapper">
+            <Image src="" circle className="profile-pic" />
+            <h3>Frank</h3>
+            <p>That's a crooked tree.</p>
+          </Col>
+          <Col xs={12} sm={4} className="person-wrapper">
+            <Image src="" circle className="profile-pic" />
+            <h3>Frank</h3>
+            <p>That's a crooked tree.</p>
+          </Col>
+          <Col xs={12} sm={4} className="person-wrapper">
+            <Image src="" circle className="profile-pic" />
+            <h3>Frank</h3>
+            <p>That's a crooked tree.</p>
+          </Col>
+        </Row>
+        
       </Container>
     )
   }
