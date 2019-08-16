97d132ad0cb4fcc4f6abdd8446d9eaa7fa812cb2
Matthew Wright <matthewbwright@gmail.com>
2019-08-15 23:48:18 -0500

    finished other pages

--- wrightlawgroup/src/Components/About.js
+++ wrightlawgroup/src/Components/About.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Container, Col, Image } from 'react-bootstrap';
+
+import './About.css';
 
 const About = () => {
   
     return (
       <div>
-        ABOUT PAGE
+        <Image src="" className="header-image" />
+        <Container>
+          <Col xs={12} sm={8} smOffset={2}>
+            <Image src="" className="about-profile-pic" rounded/>
+            <h3>Frank the Tank</h3>
+            <p>That's a crooked tree.</p>
+          </Col>
+        </Container>
       </div>
     )
   }
--- wrightlawgroup/src/Components/News.js
+++ wrightlawgroup/src/Components/News.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+
+import './News.css';
 
 const News = () => {
   
     return (
       <div>
-        NEWS PAGE
+        <Image src="" className="header-image" />
+        <Container>
+          <h2>News</h2>
+          <Row>
+            <Col xs={12} sm={8} className="main-section">
+              <p>I spend a lot of time walking around in the woods and talking to tress, and squirrels.</p>
+            </Col>
+            <Col xs={12} sm={4} className="sidebar-section">
+              <Image src=""/>
+              <p>This is sidebar section.</p>
+            </Col>
+          </Row>
+        </Container>
       </div>
     )
   }
